58c9f5a20c629c21e9062048b384f05885656316
David Degner <david@daviddegner.com>
2025-12-04 23:00:26 -0500

    Fix PHP 8.3 readonly class syntax for WordPress.org compatibility

--- includes/class-photo-collage-block-attributes.php
+++ includes/class-photo-collage-block-attributes.php
@@ -7,14 +7,15 @@
 
 declare(strict_types=1);
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 /**
  * Readonly class for block attributes
  */
-final readonly class Photo_Collage_Block_Attributes {
+final class Photo_Collage_Block_Attributes
+{
 
 
 
@@ -275,43 +276,44 @@ final readonly class Photo_Collage_Block_Attributes {
 	 *
 	 * @param array $attributes Block attributes.
 	 */
-	public function __construct( array $attributes ) {
-		$this->url         = (string) ( $attributes['url'] ?? '' );
-		$this->id          = (int) ( $attributes['id'] ?? 0 );
-		$this->href        = (string) ( $attributes['href'] ?? '' );
-		$this->link_target = (string) ( $attributes['linkTarget'] ?? '' );
-		$this->rel         = (string) ( $attributes['rel'] ?? '' );
-		$this->link_class  = (string) ( $attributes['linkClass'] ?? '' );
-		$this->alt         = (string) ( $attributes['alt'] ?? '' );
-		$this->align       = (string) ( $attributes['align'] ?? '' );
+	public function __construct(array $attributes)
+	{
+		$this->url = (string) ($attributes['url'] ?? '');
+		$this->id = (int) ($attributes['id'] ?? 0);
+		$this->href = (string) ($attributes['href'] ?? '');
+		$this->link_target = (string) ($attributes['linkTarget'] ?? '');
+		$this->rel = (string) ($attributes['rel'] ?? '');
+		$this->link_class = (string) ($attributes['linkClass'] ?? '');
+		$this->alt = (string) ($attributes['alt'] ?? '');
+		$this->align = (string) ($attributes['align'] ?? '');
 
-		$this->show_caption          = (bool) ( $attributes['showCaption'] ?? true );
-		$this->caption_align         = (string) ( $attributes['captionAlign'] ?? 'center' );
-		$this->caption_width         = (string) ( $attributes['captionWidth'] ?? '100%' );
-		$this->caption_placement     = (string) ( $attributes['captionPlacement'] ?? 'bottom-left' );
-		$this->is_decorative         = (bool) ( $attributes['isDecorative'] ?? false );
-		$this->use_absolute_position = (bool) ( $attributes['useAbsolutePosition'] ?? false );
-		$this->z_index               = (int) ( $attributes['zIndex'] ?? 1 );
-		$this->width                 = (string) ( $attributes['width'] ?? '50%' );
-		$this->height                = (string) ( $attributes['height'] ?? 'auto' );
-		$this->object_fit            = (string) ( $attributes['objectFit'] ?? 'contain' );
-		$this->rotation              = (int) ( $attributes['rotation'] ?? 0 );
-		$this->opacity               = (float) ( $attributes['opacity'] ?? 1 );
-		$this->top                   = (string) ( $attributes['top'] ?? 'auto' );
-		$this->right                 = (string) ( $attributes['right'] ?? 'auto' );
-		$this->bottom                = (string) ( $attributes['bottom'] ?? 'auto' );
-		$this->left                  = (string) ( $attributes['left'] ?? 'auto' );
-		$this->margin_top            = (string) ( $attributes['marginTop'] ?? '0%' );
-		$this->margin_right          = (string) ( $attributes['marginRight'] ?? '0%' );
-		$this->margin_bottom         = (string) ( $attributes['marginBottom'] ?? '0%' );
-		$this->margin_left           = (string) ( $attributes['marginLeft'] ?? '0%' );
-		$this->padding_top           = (string) ( $attributes['paddingTop'] ?? '0%' );
-		$this->padding_right         = (string) ( $attributes['paddingRight'] ?? '0%' );
-		$this->padding_bottom        = (string) ( $attributes['paddingBottom'] ?? '0%' );
-		$this->padding_left          = (string) ( $attributes['paddingLeft'] ?? '0%' );
-		$this->caption               = (string) ( $attributes['caption'] ?? '' );
-		$this->title                 = (string) ( $attributes['title'] ?? '' );
-		$this->description           = (string) ( $attributes['description'] ?? '' );
-		$this->lightbox              = (array) ( $attributes['lightbox'] ?? array() );
+		$this->show_caption = (bool) ($attributes['showCaption'] ?? true);
+		$this->caption_align = (string) ($attributes['captionAlign'] ?? 'center');
+		$this->caption_width = (string) ($attributes['captionWidth'] ?? '100%');
+		$this->caption_placement = (string) ($attributes['captionPlacement'] ?? 'bottom-left');
+		$this->is_decorative = (bool) ($attributes['isDecorative'] ?? false);
+		$this->use_absolute_position = (bool) ($attributes['useAbsolutePosition'] ?? false);
+		$this->z_index = (int) ($attributes['zIndex'] ?? 1);
+		$this->width = (string) ($attributes['width'] ?? '50%');
+		$this->height = (string) ($attributes['height'] ?? 'auto');
+		$this->object_fit = (string) ($attributes['objectFit'] ?? 'contain');
+		$this->rotation = (int) ($attributes['rotation'] ?? 0);
+		$this->opacity = (float) ($attributes['opacity'] ?? 1);
+		$this->top = (string) ($attributes['top'] ?? 'auto');
+		$this->right = (string) ($attributes['right'] ?? 'auto');
+		$this->bottom = (string) ($attributes['bottom'] ?? 'auto');
+		$this->left = (string) ($attributes['left'] ?? 'auto');
+		$this->margin_top = (string) ($attributes['marginTop'] ?? '0%');
+		$this->margin_right = (string) ($attributes['marginRight'] ?? '0%');
+		$this->margin_bottom = (string) ($attributes['marginBottom'] ?? '0%');
+		$this->margin_left = (string) ($attributes['marginLeft'] ?? '0%');
+		$this->padding_top = (string) ($attributes['paddingTop'] ?? '0%');
+		$this->padding_right = (string) ($attributes['paddingRight'] ?? '0%');
+		$this->padding_bottom = (string) ($attributes['paddingBottom'] ?? '0%');
+		$this->padding_left = (string) ($attributes['paddingLeft'] ?? '0%');
+		$this->caption = (string) ($attributes['caption'] ?? '');
+		$this->title = (string) ($attributes['title'] ?? '');
+		$this->description = (string) ($attributes['description'] ?? '');
+		$this->lightbox = (array) ($attributes['lightbox'] ?? array());
 	}
 }
