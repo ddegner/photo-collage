29fc5010eab34b7137292caf19a713a8736a4be0
David Degner <david@daviddegner.com>
2025-12-15 00:49:48 -0500

    Release 0.5.9: Restore readme.txt generation in deploy workflow

--- photo-collage.php
+++ photo-collage.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Photo Collage
  * Description:       Blocks for creating freeform photo layouts with more natural and chaotic structures that can overlap.
- * Version:           0.5.8
+ * Version:           0.5.9
  * Requires at least: 6.8
  * Requires PHP:      8.3
  * Author:            David Degner
@@ -23,7 +23,7 @@ if (!defined('ABSPATH')) {
 /**
  * Plugin version constant
  */
-define('PHOTO_COLLAGE_VERSION', '0.5.8');
+define('PHOTO_COLLAGE_VERSION', '0.5.9');
 
 /**
  * Registers the blocks.
